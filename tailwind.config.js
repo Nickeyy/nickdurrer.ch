5ccbf379495e21df31d7ddd6826e22fc7ea9f50f
Nick Durrer <nick_durrer@sluz.ch>
2022-11-01 13:43:48 +0100

    first version of navbar

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html', "./node_modules/flowbite/**/*.js"],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        normalBlue: '#2F74C8',
+        darkBlue: '#2169A3',
+        lightBlue: '#408FCE',
+        lightCreme: '#E6E6E4',
+        darkCreme: '#CBCBCB'
+      }
+    },
+  },
+  plugins: []
+}
